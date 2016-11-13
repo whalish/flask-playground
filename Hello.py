680ab04abdf6e2b789dd3a552a510cb506ba8566
whalish <jasonainc@berkeley.edu>
2016-11-12 16:20:42 -0800

    first commit

---
+++ Hello.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, make_response
+app = Flask(__name__)
+
+@app.route('/')
+def student():
+   return render_template('index.html')
+
+@app.route('/setcookie', methods = ['POST', 'GET'])
+def setcookie():
+   if request.method == 'POST':
+    user = request.form['nm']
+   
+   resp = make_response(render_template('result.html'))
+   resp.set_cookie('userID', user)
+   
+   return resp
+
+@app.route('/getcookie')
+def getcookie():
+   name = request.cookies.get('userID')
+   return '<h1>welcome '+name+'</h1>'
+
+if __name__ == '__main__':
+   app.run(debug = True)
